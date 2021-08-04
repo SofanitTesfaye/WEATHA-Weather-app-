b314e2341e7421300374e675e7276ed0a07cf727
Sofanit Tesfaye <sofanittesfaye@Sofanits-MacBook-Pro.local>
2021-08-04 15:01:53 -0400

    updated code

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
 // const apiKey = "b351ed20610d69f0305baa374dd8c604"
+
 let weather = {
-  apiKey: "b351ed20610d69f0305baa374dd8c604",
-  fetchWeather: function (city) {
-    fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q="
-      + city
-      + "&units=metric&APPID="
-      + this.apiKey
-    )
-      .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
-  },
+  getWeather: async function fetchWeather(city) {
+    try {
+     removeWeather();
+      const apiKey = "b351ed20610d69f0305baa374dd8c604";
+      await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      )
+        .then((response) => response.data)
+        .then((data) => this.displayWeather(data))
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+  ,
   displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed)
-    document.querySelector(".city").innerText = "weather in" + name
+    const { timezone } = data.timezone;
+    console.log(name, icon, description, temp, humidity, speed, timezone)
+    document.querySelector(".city").innerText = "weather in " + name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
     document.querySelector(".description").innerText = description
     document.querySelector(".temp").innerText = temp + "°C"
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
     document.querySelector(".speed").innerText = "Wind speed: " + speed + "km/h"
+    document.querySelector(".weather").classList.remove("loading")
+    document.querySelector(".localTime").innerText = "Local Time: " + timezone + "sec"
   },
   search: function () {
-    this.fetchWeather(document.querySelector(".searchForm").value)
-   }
+    this.getWeather(document.querySelector(".search-bar").value)
+  }
 };
+  
+function removeWeather() {
+  const removeElement = document.querySelector('.search-bar')
+  while (removeElement.lastChild) {
+  removeElement.removeChild(removeElement.lastChild)
+  }
+}
 
 document
-  .querySelector(".search button")
+  .querySelector(".searchButton")
   .addEventListener("click", function () {
     weather.search()
   })
-   
-  
+
